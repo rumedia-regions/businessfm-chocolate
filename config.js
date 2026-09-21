02a1b1acbae2ffce446806016f21312a11dc838b
rumedia-regions <vatline.90@gmail.com>
2026-09-21 11:01:25 +0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.CCR_CONFIG = Object.freeze({
-  // После deployment Apps Script вставьте сюда URL, который заканчивается на /exec.
-  // Пример: https://script.google.com/macros/s/XXXXXXXX/exec
-  appsScriptExecUrl: "PASTE_APPS_SCRIPT_EXEC_URL_HERE"
-});
